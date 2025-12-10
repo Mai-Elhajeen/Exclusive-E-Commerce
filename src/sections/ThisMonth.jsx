1aafe47bc6b8cc0b9a309b1bc08ca0adccbcb7ea
Mai-Elhajeen <maielhajeen2001>
2025-12-10 16:54:04 +0200

    feat: implement this month section

--- src/sections/ThisMonth.jsx
+++ src/sections/ThisMonth.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { HeadTitle, Title, Btn, CardProduct, Line } from "../components";
+import { products } from "../data/productsData";
+import styles from "./styles.module.css";
 const ThisMonth = () => {
-  return <div>ThisMonth</div>;
+  const start = products.id= 6;
+  const end = products.id= 10;
+  const visibleProducts = products.slice(start, end)
+  return (
+    <section className={styles.section}>
+      <HeadTitle title="This Month" />
+      <div className={styles.flex}>
+        <Title title="Best Selling Products" />
+        <Btn label="View All" space="0" />
+      </div>
+
+      <div className={styles.cards}>
+        {visibleProducts.map((product) => (
+          <CardProduct
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            oldPrice={product.oldPrice}
+            discount={product.discount}
+            badgeType={product.badgeType}
+            image={product.image}
+            rating={product.rating}
+            views={product.views}
+            showFavorite={product.showFavorite}
+            showView={product.showView}
+            showColors={product.showColors}
+            showAddToCart={product.showAddToCart}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ThisMonth;
