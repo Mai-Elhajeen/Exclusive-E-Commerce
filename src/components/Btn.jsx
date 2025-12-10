17e0a6e9d3f31c657f38a5c9dc623727e05b6be4
Mai-Elhajeen <maielhajeen2001>
2025-12-10 13:15:16 +0200

    feat: implement CardProduct component with style.module.css - create btn component reuseable - create title component reuseable

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-const Btn = () => {
-  return <div>Btn</div>;
+const Btn = ({
+  label,
+  bg = "#DB4444",
+  color = "#fff",
+  padding = "16px 48px",
+  border = "none",
+  round = "4px",
+  size = "18px",
+  flex = "flex",
+  center = "center",
+  space= "40px"
+}) => {
+  return (
+    <div style={{ display: flex, alignItems: center, justifyContent: center, margin: space }}>
+      <button
+        style={{
+          background: bg,
+          color: color,
+          padding: padding,
+          border: border,
+          outline: border,
+          borderRadius: round,
+          fontSize: size,
+        }}
+      >
+        {label}
+      </button>
+    </div>
+  );
 };
 
 export default Btn;
